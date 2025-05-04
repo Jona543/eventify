0569f9bb1d66af85b158b40ad7fc6d1fe574a24d
Jonathan Tromp <jonatromp45@gmail.com>
2025-05-04 12:26:42 +0100

    added console logs for debugging, hosting issues

--- src/auth.js
+++ src/auth.js
@@ -3,5 +3,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/authOptions';
 
 export async function auth() {
+    console.log('📦 auth.js loaded');  // Debugging
+
   return await getServerSession(authOptions);
 }
